53f95bb44e1b80bd2f15519a1b65db7572009d75
devel9826 <devel9826@gmail.com>
2026-06-19 19:15:28 +0530

    Initial commit

---
+++ bootstrap/app.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Http\Request;
+use Illuminate\Auth\AuthenticationException;
+
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php',
+        commands: __DIR__.'/../routes/console.php',
+        health: '/up',
+    )
+    ->withMiddleware(function (Middleware $middleware): void {
+        //
+        $middleware->redirectGuestsTo(
+            fn () => null
+        );
+    
+        // $middleware->api(prepend: [
+        //     \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        // ]);
+    })
+    ->withExceptions(function (Exceptions $exceptions): void {
+        $exceptions->shouldRenderJsonWhen(
+            fn (Request $request) => $request->is('api/*'),
+        );
+        $exceptions->render(
+            function (
+                AuthenticationException $e,
+                Request $request
+            ) {
+    
+                if ($request->is('api/*')) {
+                    return response()->json([
+                        'message' => 'Unauthenticated'
+                    ], 401);
+                }
+    
+            }
+        );
+    })->create();
